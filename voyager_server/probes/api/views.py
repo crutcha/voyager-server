470ebafdef551029cfbb6b8c5e78ae8d77a7ec60
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2020-08-04 00:26:54 -0400

    somewhat working example

--- voyager_server/probes/api/views.py
+++ voyager_server/probes/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from voyager_server.probes.models import ProbeTarget, ProbeResult, ProbeHop
 from voyager_server.probes.api import serializers
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action, renderer_classes
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 
 class ProbeTargetViewSet(viewsets.ModelViewSet):
@@ -17,6 +20,35 @@ class ProbeResultViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = ProbeResult.objects.all()
     serializer_class = serializers.ProbeResultSerializer
+
+    @action(detail=True)
+    @renderer_classes([JSONRenderer])
+    def jsgraph(self, request, pk=None):
+        result = ProbeResult.objects.get(pk=pk)
+
+        # This probably isn't the most efficient, but for now we'll do 2 passes
+        # through the results to create d3js friendly graph. O(2n), could be worse...
+        data = {"nodes": [], "links": []}
+        hop_map = []
+        filtered_results = result.hops.distinct("ip", "ttl").order_by("ttl")
+        for hop in filtered_results:
+            print(hop.__dict__)
+            if not hop.ip:
+                hop.ip = ""
+            data["nodes"].append({"id": hop.id, "name": hop.ip, "dns": hop.dns_name})
+            if len(hop_map) <= hop.ttl - 1:
+                hop_map.append({hop.ip: hop.id})
+            else:
+                if not hop.ip in hop_map[hop.ttl - 1]:
+                    hop_map[hop.ttl - 1][hop.ip] = hop.id
+
+        # Connect every downstream node in directed graph
+        for index in range(0, len(hop_map) - 1):
+            for from_ip, from_id in hop_map[index].items():
+                for to_ip, to_id in hop_map[index + 1].items():
+                    data["links"].append({"source": from_id, "target": to_id})
+
+        return Response(data)
 
 
 class ProbeHopViewSet(viewsets.ModelViewSet):
