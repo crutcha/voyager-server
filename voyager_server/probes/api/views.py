dbbc255c6a244f399b4b3bcd1292d6d734537433
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2020-08-04 23:22:25 -0400

    trying visjs

--- voyager_server/probes/api/views.py
+++ voyager_server/probes/api/views.py
@@ -28,14 +28,13 @@ class ProbeResultViewSet(viewsets.ModelViewSet):
 
         # This probably isn't the most efficient, but for now we'll do 2 passes
         # through the results to create d3js friendly graph. O(2n), could be worse...
-        data = {"nodes": [], "links": []}
+        data = {"nodes": [], "edges": []}
         hop_map = []
         filtered_results = result.hops.distinct("ip", "ttl").order_by("ttl")
         for hop in filtered_results:
-            print(hop.__dict__)
             if not hop.ip:
                 hop.ip = ""
-            data["nodes"].append({"id": hop.id, "name": hop.ip, "dns": hop.dns_name})
+            data["nodes"].append({"id": hop.id, "label": hop.dns_name})
             if len(hop_map) <= hop.ttl - 1:
                 hop_map.append({hop.ip: hop.id})
             else:
@@ -46,7 +45,7 @@ class ProbeResultViewSet(viewsets.ModelViewSet):
         for index in range(0, len(hop_map) - 1):
             for from_ip, from_id in hop_map[index].items():
                 for to_ip, to_id in hop_map[index + 1].items():
-                    data["links"].append({"source": from_id, "target": to_id})
+                    data["edges"].append({"from": from_id, "to": to_id})
 
         return Response(data)
 
